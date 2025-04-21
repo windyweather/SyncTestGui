5c44281ff402a6700320006c5bb47b8175c0dd63
Windy Player <windyplayer@gmx.com>
2025-04-21 09:14:31 -0700

    Files for reported bug in Scene Builder

--- src/main/java/net/windyweather/synctestgui/STGController.java
+++ src/main/java/net/windyweather/synctestgui/STGController.java
@@ -2,8 +2,15 @@ package net.windyweather.synctestgui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TreeTableColumn;
+import javafx.scene.control.TreeTableView;
 
 public class STGController {
+    public TreeTableView tvFileTree;
+    public TreeTableColumn tcSourcePath;
+    public TreeTableColumn tcFileSize;
+    public TreeTableColumn tcActionPending;
+    public TreeTableColumn tcStatus;
     @FXML
     private Label welcomeText;
 
